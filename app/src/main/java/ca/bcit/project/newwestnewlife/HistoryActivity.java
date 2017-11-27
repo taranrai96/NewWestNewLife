ec2b6899f40abd0e2400ea8196ca2c44d63cad71
taranrai96 <taranrai96@gmail.com>
2017-11-27 12:07:19 -0800

    updating history_image rendering + minor fixes

--- app/src/main/java/ca/bcit/project/newwestnewlife/HistoryActivity.java
+++ app/src/main/java/ca/bcit/project/newwestnewlife/HistoryActivity.java
@@ -7,8 +7,14 @@
 
 package ca.bcit.project.newwestnewlife;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 
 import com.xenione.library.BeforeAfterCoordinatorLayout;
 
@@ -19,25 +25,72 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        BeforeAfterCoordinatorLayout beforeAfterCoordinatorLayout_1 = (BeforeAfterCoordinatorLayout) findViewById(R.id.image1);
+        try {
 
-        beforeAfterCoordinatorLayout_1.setForegroundImage(R.drawable.history_1_1);
-        beforeAfterCoordinatorLayout_1.setBackgroundImage(R.drawable.history_1_2);
+            BeforeAfterCoordinatorLayout beforeAfterCoordinatorLayout_1 = (BeforeAfterCoordinatorLayout) findViewById(R.id.image1);
 
-        BeforeAfterCoordinatorLayout beforeAfterCoordinatorLayout_2 = (BeforeAfterCoordinatorLayout) findViewById(R.id.image2);
+            beforeAfterCoordinatorLayout_1.setForegroundImage(resizeImage(R.drawable.history_1_1));
+            beforeAfterCoordinatorLayout_1.setBackgroundImage(resizeImage(R.drawable.history_1_2));
 
-        beforeAfterCoordinatorLayout_2.setForegroundImage(R.drawable.history_7_1);
-        beforeAfterCoordinatorLayout_2.setBackgroundImage(R.drawable.history_7_2);
+            BeforeAfterCoordinatorLayout beforeAfterCoordinatorLayout_2 = (BeforeAfterCoordinatorLayout) findViewById(R.id.image2);
 
-        BeforeAfterCoordinatorLayout beforeAfterCoordinatorLayout_3 = (BeforeAfterCoordinatorLayout) findViewById(R.id.image3);
+            beforeAfterCoordinatorLayout_2.setForegroundImage(resizeImage(R.drawable.history_7_1));
+            beforeAfterCoordinatorLayout_2.setBackgroundImage(resizeImage(R.drawable.history_7_2));
 
-        beforeAfterCoordinatorLayout_3.setForegroundImage(R.drawable.history_4_1);
-        beforeAfterCoordinatorLayout_3.setBackgroundImage(R.drawable.history_4_2);
+            BeforeAfterCoordinatorLayout beforeAfterCoordinatorLayout_3 = (BeforeAfterCoordinatorLayout) findViewById(R.id.image3);
 
-        BeforeAfterCoordinatorLayout beforeAfterCoordinatorLayout_4 = (BeforeAfterCoordinatorLayout) findViewById(R.id.image4);
+            beforeAfterCoordinatorLayout_3.setForegroundImage(resizeImage(R.drawable.history_4_1));
+            beforeAfterCoordinatorLayout_3.setBackgroundImage(resizeImage(R.drawable.history_4_2));
 
-        beforeAfterCoordinatorLayout_4.setForegroundImage(R.drawable.history_10_1);
-        beforeAfterCoordinatorLayout_4.setBackgroundImage(R.drawable.history_10_2);
+            BeforeAfterCoordinatorLayout beforeAfterCoordinatorLayout_4 = (BeforeAfterCoordinatorLayout) findViewById(R.id.image4);
 
+            beforeAfterCoordinatorLayout_4.setForegroundImage(resizeImage(R.drawable.history_10_1));
+            beforeAfterCoordinatorLayout_4.setBackgroundImage(resizeImage(R.drawable.history_10_2));
+
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Drawable resizeImage(int imageResource) {// R.drawable.icon
+        // Get device dimensions
+        Display display = getWindowManager().getDefaultDisplay();
+        double deviceWidth = display.getWidth();
+
+        BitmapDrawable bd = (BitmapDrawable) this.getResources().getDrawable(
+                imageResource);
+        double imageHeight = bd.getBitmap().getHeight();
+        double imageWidth = bd.getBitmap().getWidth();
+
+        double ratio = deviceWidth / imageWidth;
+        int newImageHeight = (int) (imageHeight * ratio);
+
+        Bitmap bMap = BitmapFactory.decodeResource(getResources(), imageResource);
+        Drawable drawable = new BitmapDrawable(this.getResources(),
+                getResizedBitmap(bMap, newImageHeight, (int) deviceWidth));
+
+        return drawable;
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+
+        // create a matrix for the manipulation
+        Matrix matrix = new Matrix();
+
+        // resize the bit map
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // recreate the new Bitmap
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height,
+                matrix, false);
+
+        return resizedBitmap;
     }
 }
